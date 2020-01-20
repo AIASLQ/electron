c42d493484d932a1091236a65cd4a14bfd5117bc
slq <slq@iplusu>
2020-01-20 17:56:43 +0800

    第一次提交

---
+++ renderer.js
@@ -0,0 +1,23 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+const { ipcRenderer } = require('electron')
+const { BrowserWindow } = require('electron').remote
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('node-version').innerHTML = process.versions.node
+    document.getElementById('send').addEventListener('click', () => {
+        ipcRenderer.send('message', 'hello form render')
+        let win = new BrowserWindow({
+            width: 800,
+            height: 600
+        })
+        win.loadURL('https://baidu.com')
+    })
+    ipcRenderer.on('reply', (event, arg) => {
+        document.getElementById('message').innerHTML = arg
+    })
+})
